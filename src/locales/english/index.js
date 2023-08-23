bd4398126ac15dc18d58357a1ecaf2c2ec29fdd6
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2023-08-24 02:29:30 +0500

    English labels

---
+++ src/locales/english/index.js
@@ -0,0 +1,12 @@
+const en = {
+  home: "home",
+  aboutUs: "about us",
+  contactUS: "contact us",
+  createPost: "create post",
+  whereAImeetsHumanCreativity: "Where AI meets human creativity.",
+  YourSourceForInspirationAndAIGeneratedImages:
+    "Your source for inspiration and AI-generated images.",
+  lookForAIGeneratedVisuals: "Look for AI-generated visuals.",
+  copyRight: `imaginaria © ${new Date().getFullYear()} All rights reserved`,
+};
+export default en;
