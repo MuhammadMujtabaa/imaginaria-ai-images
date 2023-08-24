9a051eb32730c04643967d63f65e0015b1542962
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2023-08-25 00:01:11 +0500

    contact us screen

--- src/pages/ContactUs/index.jsx
+++ src/pages/ContactUs/index.jsx
@@ -1,5 +1,4 @@
-import { Button } from "../../components/formComponents";
-
+import { Button, TextArea, TextInput } from "../../components/formComponents";
 const ContactUS = () => {
   return (
     <div className="relative isolate bg-white">
@@ -35,16 +34,16 @@ const ContactUS = () => {
                 />
               </svg>
             </div>
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 font-Poppins_bold">
               Get in touch
             </h2>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
+            <p className="mt-6 text-lg leading-8 text-gray-600 font-Poppins_regular">
               Proin volutpat consequat porttitor cras nullam gravida at. Orci
               molestie a eu arcu. Sed ut tincidunt integer elementum id sem.
               Arcu sed malesuada et magna.
             </p>
             <dl className="mt-10 space-y-4 text-base leading-7 text-gray-600">
-              <div className="flex gap-x-4">
+              <div className="flex gap-x-4 font-Poppins_regular">
                 <dt className="flex-none">
                   <span className="sr-only">Address</span>
                   {/* <BuildingOffice2Icon className="h-7 w-6 text-gray-400" aria-hidden="true" /> */}
@@ -55,7 +54,7 @@ const ContactUS = () => {
                   Chicago, IL 99191
                 </dd>
               </div>
-              <div className="flex gap-x-4">
+              <div className="flex gap-x-4 font-Poppins_regular">
                 <dt className="flex-none">
                   <span className="sr-only">Telephone</span>
                   {/* <PhoneIcon className="h-7 w-6 text-gray-400" aria-hidden="true" /> */}
@@ -69,14 +68,14 @@ const ContactUS = () => {
                   </a>
                 </dd>
               </div>
-              <div className="flex gap-x-4">
+              <div className="flex gap-x-4 font-Poppins_regular">
                 <dt className="flex-none">
                   <span className="sr-only">Email</span>
                   {/* <EnvelopeIcon className="h-7 w-6 text-gray-400" aria-hidden="true" /> */}
                 </dt>
                 <dd>
                   <a
-                    className="hover:text-gray-900"
+                    className="hover:text-gray-900 font-Poppins_regular"
                     href="mailto:hello@example.com"
                   >
                     hello@example.com
@@ -87,113 +86,37 @@ const ContactUS = () => {
           </div>
         </div>
         <form
-          action="#"
-          method="POST"
           className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48"
+          onSubmit={(e) => e.preventDefault()}
         >
           <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
-            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <div className="grid grid-cols-1 gap-x-8 gap-y-2 sm:grid-cols-2">
               <div>
-                <label
-                  htmlFor="first-name"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  First name
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    type="text"
-                    name="first-name"
-                    id="first-name"
-                    autoComplete="given-name"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
+                <TextInput label="First name" name="first-name" />
               </div>
               <div>
-                <label
-                  htmlFor="last-name"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Last name
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    type="text"
-                    name="last-name"
-                    id="last-name"
-                    autoComplete="family-name"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
+                <TextInput label="Last name" name="last-name" />
               </div>
               <div className="sm:col-span-2">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Email
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    autoComplete="email"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
+                <TextInput label="Email" name="email" />
               </div>
               <div className="sm:col-span-2">
-                <label
-                  htmlFor="phone-number"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Phone number
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    type="tel"
-                    name="phone-number"
-                    id="phone-number"
-                    autoComplete="tel"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
+                <TextInput label="Phone number" name="phone-number" />
               </div>
               <div className="sm:col-span-2">
-                <label
-                  htmlFor="message"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Message
-                </label>
-                <div className="mt-2.5">
-                  <textarea
-                    name="message"
-                    id="message"
-                    rows={4}
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    defaultValue={""}
-                  />
-                </div>
+                <TextArea label="Message" name="message" />
               </div>
             </div>
-            <div className="mt-8 flex justify-end">
+            <div className="mt-5 flex justify-end">
               <Button
                 onClick={() => {}}
                 bgColor={"bg-black ring-[black]"}
                 textColor={"text-[white]"}
                 size="md"
+                type="submit"
               >
                 Sign Up – It's Free
               </Button>
-              {/* <button
-                type="submit"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Send message
-              </button> */}
             </div>
           </div>
         </form>
