f3fa2b350bd166c13882507eb55e7e449e84463d
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2023-08-26 00:52:54 +0500

    chore: imaginaria logo is added

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,6 @@
-import logo from "./image/imaginaria.png"
+import logo from "./image/imaginaria.png";
+import imaginaria from "./image/imaginaria1.png";
 export const Images = {
   logo,
+  imaginaria
 };
