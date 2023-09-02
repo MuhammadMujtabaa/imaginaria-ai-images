a3780099550132dcd5ed683039bf38fc63ae00dd
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2023-09-02 14:30:13 +0500

    feat: new images are added

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,47 @@
 import logo from "./image/imaginaria.png";
 import imaginaria from "./image/imaginaria1.png";
+import Imaginaria1 from "./image/Imaginaria-1.jpg";
+import Imaginaria2 from "./image/Imaginaria-2.jpg";
+import Imaginaria3 from "./image/Imaginaria-3.jpg";
+import Imaginaria4 from "./image/Imaginaria-4.jpg";
+import Imaginaria5 from "./image/Imaginaria-5.jpg";
+import Imaginaria6 from "./image/Imaginaria-6.jpg";
+import Imaginaria7 from "./image/Imaginaria-7.jpg";
+import Imaginaria8 from "./image/Imaginaria-8.jpg";
+import Imaginaria9 from "./image/Imaginaria-9.jpg";
+import Imaginaria10 from "./image/Imaginaria-10.jpg";
+import Imaginaria11 from "./image/Imaginaria-11.jpg";
+import Imaginaria12 from "./image/Imaginaria-12.jpg";
+import Imaginaria13 from "./image/Imaginaria-13.jpg";
+import Imaginaria14 from "./image/Imaginaria-14.jpg";
+import Imaginaria15 from "./image/Imaginaria-15.jpg";
+import Imaginaria16 from "./image/Imaginaria-16.jpg";
+import Imaginaria17 from "./image/Imaginaria-17.jpg";
+import Imaginaria18 from "./image/Imaginaria-18.jpg";
+import Imaginaria19 from "./image/Imaginaria-19.jpg";
+import Imaginaria20 from "./image/Imaginaria-20.jpg";
+
 export const Images = {
   logo,
-  imaginaria
+  imaginaria,
+  Imaginaria1,
+  Imaginaria2,
+  Imaginaria3,
+  Imaginaria4,
+  Imaginaria5,
+  Imaginaria6,
+  Imaginaria7,
+  Imaginaria8,
+  Imaginaria9,
+  Imaginaria10,
+  Imaginaria11,
+  Imaginaria12,
+  Imaginaria13,
+  Imaginaria14,
+  Imaginaria15,
+  Imaginaria16,
+  Imaginaria17,
+  Imaginaria18,
+  Imaginaria19,
+  Imaginaria20,
 };
