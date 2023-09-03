2feb8a48a7298b9672b5b0dc8efbb1c4939439c2
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2023-09-03 23:59:48 +0500

    chore: updated

--- src/components/pagesComponents/homePage/MainSearchSection/index.jsx
+++ src/components/pagesComponents/homePage/MainSearchSection/index.jsx
@@ -28,28 +28,8 @@ const MainSearchSection = () => {
         alt=""
         className="absolute inset-0 -z-10 h-full w-full object-cover bg-black bg-opacity-30"
       />
-      {/* <div
-        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        aria-hidden="true"
-      >
-        <div
-          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        />
-      </div> */}
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        {/* <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-      <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
-        Announcing our next round of funding.{" "}
-        <a href="#" className="font-semibold text-white">
-          <span className="absolute inset-0" aria-hidden="true" />
-          Read more <span aria-hidden="true">&rarr;</span>
-        </a>
-      </div>
-    </div> */}
+
+      <div className="mx-auto max-w-2xl sm:py-48 lg:py-56 px-3 pt-40 pb-10">
         <div className="text-center">
           <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl font-Poppins_bold">
             Imaginaria
@@ -59,20 +39,7 @@ const MainSearchSection = () => {
             <br />
             Your source for inspiration and AI-generated images.
           </p>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
-            {/* <a
-          href="#"
-          className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
-        >
-          Get started
-        </a>
-        <a
-          href="#"
-          className="text-sm font-semibold leading-6 text-white"
-        >
-          Learn more <span aria-hidden="true">→</span>
-        </a> */}
-          </div>
+          <div className="mt-10 flex items-center justify-center gap-x-6"></div>
           <div className="w-full relative h-12">
             <input
               className="p-4 h-12 bg-white w-full rounded outline-none font-Poppins_regular bg-opacity-90"
@@ -83,9 +50,6 @@ const MainSearchSection = () => {
             </div>
           </div>
         </div>
-        {/* <div className="hidden sm:mb-8 sm:flex sm:justify-center mt-3">
-      <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">All </div>
-    </div> */}
       </div>
       {/* <div
         className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
