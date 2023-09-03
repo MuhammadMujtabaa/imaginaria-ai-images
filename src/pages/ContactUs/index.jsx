0b8150583bd6f18fb958ebaa6767f229bd4f2f17
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2023-09-03 16:49:37 +0500

    chore: contact us page content updated

--- src/pages/ContactUs/index.jsx
+++ src/pages/ContactUs/index.jsx
@@ -3,7 +3,10 @@ const ContactUS = () => {
   return (
     <div className="relative isolate bg-white">
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
-        <div className="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
+        <div
+          className="relative px-6 pb-20 pt-36 sm:pt-60 lg:static lg:px-8 lg:py-48"
+          // className="mx-auto max-w-7xl px-6 pt-36 sm:pt-60 lg:px-0 lg:pt-32"
+        >
           <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
             <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 lg:w-1/2">
               <svg
@@ -34,7 +37,7 @@ const ContactUS = () => {
                 />
               </svg>
             </div>
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 font-Poppins_bold">
+            <h2 className="text-2xl font-bold tracking-tight text-[#000000] sm:text-4xl font-Poppins_medium">
               Get in touch
             </h2>
             <p className="mt-6 text-lg leading-8 text-gray-600 font-Poppins_regular">
@@ -49,25 +52,12 @@ const ContactUS = () => {
                   {/* <BuildingOffice2Icon className="h-7 w-6 text-gray-400" aria-hidden="true" /> */}
                 </dt>
                 <dd>
-                  545 Mavis Island
+                  269 South Beverly Drive, #1234
                   <br />
-                  Chicago, IL 99191
+                  Beverly Hills, CA 90212
                 </dd>
               </div>
-              <div className="flex gap-x-4 font-Poppins_regular">
-                <dt className="flex-none">
-                  <span className="sr-only">Telephone</span>
-                  {/* <PhoneIcon className="h-7 w-6 text-gray-400" aria-hidden="true" /> */}
-                </dt>
-                <dd>
-                  <a
-                    className="hover:text-gray-900"
-                    href="tel:+1 (555) 234-5678"
-                  >
-                    +1 (555) 234-5678
-                  </a>
-                </dd>
-              </div>
+
               <div className="flex gap-x-4 font-Poppins_regular">
                 <dt className="flex-none">
                   <span className="sr-only">Email</span>
@@ -76,9 +66,9 @@ const ContactUS = () => {
                 <dd>
                   <a
                     className="hover:text-gray-900 font-Poppins_regular"
-                    href="mailto:hello@example.com"
+                    href="mailto:hello@imaginaria.ai"
                   >
-                    hello@example.com
+                    hello@imaginaria.ai
                   </a>
                 </dd>
               </div>
